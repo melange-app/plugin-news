a684bb0f3d1b1ebb1ff564cc00f044818d146ebc
Hunter Leath <h@hunterleath.com>
2014-09-28 09:13:13 -0400

    News is now able to download its information over AD.

--- news.js
+++ news.js
@@ -1,2 +1,14 @@
 $(document).ready(function() {
+  var currentURL = "http://nytimes.com";
+
+  melange.downloadMessage("news@airdispatch.me", "latest", function(msg) {
+    console.log(msg);
+    $("#title").html(msg.components["airdispat.ch/news/headline"].string);
+    $('#background').css('background-image', 'url(http://data.melange:7776/' + msg.components["airdispat.ch/news/image"].string + ')');
+    currentURL = msg.components["airdispat.ch/news/url"].string
+  });
+
+  $("#background *").click(function() {
+    melange.openLink(currentURL);
+  });
 });
